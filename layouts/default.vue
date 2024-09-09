6c8d3a979457b3f5cc456dc491d07fef7bdf377d
hankyang <73307hank@gmail.com>
2024-09-09 14:30:01 +0800

    手機版登入頁

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="w-full relative" v-loading="globalLoading">
+  <div
+    class="w-full relative"
+    v-loading="globalLoading"
+    element-loading-text="Loading..."
+    :element-loading-spinner="loadingIcon"
+    element-loading-svg-view-box="-10, -10, 50, 50"
+    element-loading-background="rgba(122, 122, 122, 0.8)"
+  >
     <Header :scrollOver="scrollOver" @openUserDialog="openUserDialog" />
     <div
       class="layout w-full mx-auto text-center"
@@ -9,7 +16,11 @@
     </div>
     <Footer class="sticky bottom-0 left-0 bg-[#000]" />
     <!-- 註冊/登入選單 -->
-    <SignInAndSignup :signDialog="signDialogStatus" @close="closeUserDialog" />
+    <SignInAndSignup
+      v-if="deviceName === 'unknown'"
+      :signDialog="signDialogStatus"
+      @close="closeUserDialog"
+    />
     <GlobalAlert />
   </div>
 </template>
@@ -23,6 +34,18 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 }
 
 import SignInAndSignup from "../components/SignInAndSignup.vue";
+import deviceName from "../util/mobileDetective";
+
+const loadingIcon = `
+  <path class="path" d="
+    M 30 15
+    L 28 17
+    M 25.61 25.61
+    A 15 15, 0, 0, 1, 15 30
+    A 15 15, 0, 1, 1, 27.99 7.5
+    L 15 15
+  " style="stroke-width: 4px; fill: rgba(0, 0, 0, 0)"/>
+`;
 
 const initialStore = useInitialStore();
 const { handleSignDialog } = initialStore;
--- pages/Login.vue
+++ pages/Login.vue
@@ -1,16 +1,82 @@
 <template>
   <div class="px-2 w-full">
     <div class="pt-[8vh]">
-      <img
-        src="/logo.png"
-        alt="logo"
-        class="inline-block xl:hidden w-[150px]"
-      />
+      <img src="/logo.png" alt="logo" class="inline-block w-[150px]" />
       <h2 class="text-3xl font-bold text-white pt-3">MEME TALK</h2>
+    </div>
+    <div
+      class="md:px-[45px] xl:py-[75px] md:py-[50px] text-white flex flex-col items-center bg-white rounded"
+    >
+      <div>
+        <h2 class="my-2 text-2xl font-bold text-black">遊客登入</h2>
+        <p class="my-2 text-sm text-black">按讚可暫時可儲存貼文</p>
+      </div>
+      <el-form
+        :model="registerForm"
+        @submit.prevent="onRegister(registerRef)"
+        :rules="registerRules"
+        ref="registerRef"
+        class="w-1/2 sm:w-[30vw] max-w-[380px]"
+      >
+        <el-form-item prop="nickname">
+          <el-input
+            v-model="registerForm.nickname"
+            placeholder="暱稱"
+            :max="3"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            type="danger"
+            native-type="submit"
+            class="w-full mt-4"
+            :disabled="globalLoading || isLogin"
+          >
+            創建暱稱
+          </el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const userStore = useUserStore();
+const { setNickname } = userStore;
+const { isLogin } = storeToRefs(userStore);
+
+const initialStore = useInitialStore();
+const { globalLoading } = storeToRefs(initialStore);
+
+const router = useRouter();
+
+const registerRef = ref();
+
+const registerRules = reactive({
+  nickname: [{ required: true, message: "請輸入暱稱", trigger: "blur" }],
+});
+
+let registerForm = reactive({
+  nickname: "",
+});
+
+const onRegister = async (formEl) => {
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      setNickname(registerForm.nickname);
+      registerForm.nickname = "";
+    } else {
+      console.log("error submit!", fields);
+    }
+  });
+};
+
+watch(isLogin, async (status) => {
+  if (status) {
+    router.push({ path: "/" });
+  }
+});
+</script>
 
 <style scoped></style>
