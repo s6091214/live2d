c2e9e529d5fe775ece3ad496386a19120bdb6bbd
hankyang <73307hank@gmail.com>
2024-09-12 08:35:51 +0800

    按讚邏輯(待測試)

--- pages/PersonSpace.vue
+++ pages/PersonSpace.vue
@@ -15,17 +15,46 @@
 <script lang="ts" setup>
 const memeStore = useMemeStore();
 const { getMemeList } = memeStore;
-const { memeList } = storeToRefs(memeStore);
+const { memeList, hotMemesList } = storeToRefs(memeStore);
 
 const userStore = useUserStore();
-const { isLogin, likeIdList } = storeToRefs(userStore);
+const { isLogin, likeIdList, isGoogleLogin, userInfo } = storeToRefs(userStore);
+
+const googleUid = computed(() => {
+  if (isGoogleLogin) {
+    return userInfo.value.uid;
+  }
+  return null;
+});
 
 const filterLikes = computed(() => {
-  if (likeIdList?.value && memeList?.value) {
-    const result = [...memeList.value];
-    return result.filter((item) => likeIdList.value.includes(item.id));
+  let result = [];
+  if (likeIdList?.value) {
+    if (memeList?.value) {
+      result = [...memeList.value].filter((item) =>
+        likeIdList.value.includes(item.id)
+      );
+    }
+    if (hotMemesList?.value && isGoogleLogin) {
+      let addList = [];
+      hotMemesList.value.map((meme) => {
+        let parseLikeList = [];
+        try {
+          parseLikeList = JSON.parse(meme.liked_user);
+        } catch (error) {}
+        if (
+          parseLikeList.length &&
+          parseLikeList.includes(googleUid.value) &&
+          !likeIdList.value.includes(meme.id)
+        ) {
+          addList.push(meme);
+        }
+        return meme;
+      });
+      result = result.concat(addList);
+    }
   }
-  return [];
+  return result;
 });
 
 if (!memeList.value.length) {
